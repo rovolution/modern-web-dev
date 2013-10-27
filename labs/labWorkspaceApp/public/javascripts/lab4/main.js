fbd77066b61298607a5d2bdd5d78850fc9e18ee7
Rohit Kalkur <rkalkur@mitre.org>
2013-10-27 18:21:29 -0400

    finishing up angular js lab

---
+++ labs/labWorkspaceApp/public/javascripts/lab4/main.js
@@ -0,0 +1,17 @@
+/******************************
+	 	
+	 Angular Controllers 
+
+*******************************/
+function EmployeeManagementViewCtrl($scope) {
+	$scope.triggerRandomBtnAction = function() {
+		alert("Random Button was clicked!!");
+	}
+}
+
+
+/******************************
+	 	
+	 Angular Resources
+
+*******************************/
